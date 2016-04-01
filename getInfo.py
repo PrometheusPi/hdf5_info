69c971b98165a293d5ea0ed2484ad4e5bb252b58
PrometheusPi <r.pausch@hzdr.de>
2016-04-01 13:35:07 +0200

    adjust interface to handle key errors internally

--- getInfo.py
+++ getInfo.py
@@ -1,30 +1,38 @@
 import numpy as np
 import h5py
 
-def getInfo(h5Pointer):
+def getInfo(h5File, key):
     """
     prints either items and attributes or values and attributes
     of a h5 pointer to screen.
 
     Input:
-    h5Pointer - h5py object
+    h5File - hdf5 file (h5py object)
+    key    - key to h5py object
 
     Output:
     none
     """
     space = "    "
+    print("KEY: {}".format(key))
     print("ITEMS:")
     try:
         # in case it is a group with datat sets
-        for i in list(h5Pointer.items()):
+        for i in list(h5File[key].items()):
             print(space + "{:16s}\t{}".format(i[0], i[1]))
     except AttributeError:
         # in case it is a data set
         print(space + "'Dataset' object has no attribute 'items'")
-        print(space + "np.shape = {}".format(np.shape(h5Pointer)))
+        print(space + "np.shape = {}".format(np.shape(h5File[key])))
         print(space + "Raw Data:")
-        print("{}".format(h5Pointer.value))
+        print("{}".format(h5File[key].value))
+
+    except KeyError:
+        print("The given key ({}) does not exist.".format(key))
+        return
         
     print("\nATTRIBUTS:")
-    for i in list(h5Pointer.attrs.items()):
+    for i in list(h5File[key].attrs.items()):
         print(space + "{:16s}\t{}".format(i[0], i[1]))
+
+    return
